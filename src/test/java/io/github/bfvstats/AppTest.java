186cb0c98c1cb77b62cc25fa864f3adeb8f30658
bfstats <bfstatsrefractor@gmail.com>
2016-12-22 00:04:41 +0200

    Add "Hello world" integration test

---
+++ src/test/java/io/github/bfvstats/AppTest.java
@@ -0,0 +1,28 @@
+package io.github.bfvstats;
+
+import com.jayway.restassured.response.Response;
+import org.junit.Rule;
+import org.junit.Test;
+import ro.pippo.core.HttpConstants;
+import ro.pippo.test.PippoRule;
+import ro.pippo.test.PippoTest;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class AppTest extends PippoTest {
+
+  @Rule
+  public PippoRule pippoRule = new PippoRule(new BasicApplication());
+
+  @Test
+  public void testHello() {
+    Response response = get("/");
+
+    response.then()
+        .statusCode(200)
+        .contentType(HttpConstants.ContentType.TEXT_HTML);
+
+    assertThat(response.asString(), equalTo("Hello World"));
+  }
+}
