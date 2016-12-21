278590c6fa83a60370c4e76793ea7d48ccd65e0f
bfstats <bfstatsrefractor@gmail.com>
2016-12-22 01:32:09 +0200

    Set environment to dev by default. This will also disable pebble (template engine) cache. Still need to recompile the template though, because currently pippo takes it from the classpath.

--- src/main/java/io/github/bfvstats/Launcher.java
+++ src/main/java/io/github/bfvstats/Launcher.java
@@ -1,9 +1,12 @@
 package io.github.bfvstats;
 
 import ro.pippo.core.Pippo;
+import ro.pippo.core.PippoConstants;
+import ro.pippo.core.RuntimeMode;
 
 public class Launcher {
   public static void main(String[] args) {
+    System.setProperty(PippoConstants.SYSTEM_PROPERTY_PIPPO_MODE, RuntimeMode.DEV.toString());
     Pippo pippo = new Pippo(new BasicApplication());
     pippo.start();
   }
