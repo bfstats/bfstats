1d5d298d392f35c7204307b1102679aeb657a4e7
bfstats <bfstatsrefractor@gmail.com>
2016-12-22 03:38:46 +0200

    pebble for loop

--- src/main/java/io/github/bfvstats/service/InMemoryContactService.java
+++ src/main/java/io/github/bfvstats/service/InMemoryContactService.java
@@ -9,13 +9,16 @@ public class InMemoryContactService implements ContactService {
 
   @Override
   public List<Contact> getContacts() {
-    return new ArrayList<>();
+    List<Contact> contacts = new ArrayList<>();
+    contacts.add(getContact(1));
+    contacts.add(getContact(2));
+    return contacts;
   }
 
   @Override
   public Contact getContact(int id) {
     return new Contact()
-        .setId(12345)
+        .setId(id)
         .setName("John")
         .setPhone("0733434435")
         .setAddress("Sunflower Street, No. 6");
