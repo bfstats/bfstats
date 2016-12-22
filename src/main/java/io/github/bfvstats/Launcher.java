26f035702be13e715f65e6f7f56188e09610c14b
bfstats <bfstatsrefractor@gmail.com>
2016-12-22 21:21:00 +0200

    Hide pippo and jOOQ logos from sout and logs

--- src/main/java/io/github/bfvstats/Launcher.java
+++ src/main/java/io/github/bfvstats/Launcher.java
@@ -7,6 +7,8 @@ import ro.pippo.core.RuntimeMode;
 public class Launcher {
   public static void main(String[] args) {
     System.setProperty(PippoConstants.SYSTEM_PROPERTY_PIPPO_MODE, RuntimeMode.DEV.toString());
+    System.setProperty("pippo.hideLogo", "true");
+    System.setProperty("org.jooq.no-logo", "true");
     Pippo pippo = new Pippo(new BasicApplication());
     pippo.start();
   }
