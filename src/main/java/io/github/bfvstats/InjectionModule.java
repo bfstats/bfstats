49bbacca7ee2324890247f5ab7c5336503170775
bfstats <bfstatsrefractor@gmail.com>
2016-12-26 03:03:25 +0200

    Remove unused InMemoryPlayerService

--- src/main/java/io/github/bfvstats/InjectionModule.java
+++ src/main/java/io/github/bfvstats/InjectionModule.java
@@ -2,7 +2,6 @@ package io.github.bfvstats;
 
 import com.google.inject.AbstractModule;
 import io.github.bfvstats.service.ChatService;
-import io.github.bfvstats.service.DatabasePlayerService;
 import io.github.bfvstats.service.PlayerService;
 import io.github.bfvstats.service.RankingService;
 import ro.pippo.core.Application;
@@ -11,7 +10,7 @@ public class InjectionModule extends AbstractModule {
 
   @Override
   protected void configure() {
-    bind(PlayerService.class).to(DatabasePlayerService.class).asEagerSingleton();
+    bind(PlayerService.class).asEagerSingleton();
     bind(RankingService.class).asEagerSingleton();
     bind(ChatService.class).asEagerSingleton();
     bind(Application.class).to(BasicApplication.class).asEagerSingleton();
--- src/main/java/io/github/bfvstats/service/PlayerService.java
+++ src/main/java/io/github/bfvstats/service/PlayerService.java
@@ -1,11 +1,34 @@
 package io.github.bfvstats.service;
 
-import io.github.bfvstats.Player;
 
+import io.github.bfvstats.Player;
+import io.github.bfvstats.jpa.tables.records.SelectbfPlayersRecord;
+import org.jooq.Result;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public interface PlayerService {
-  List<Player> getPlayers();
+import static io.github.bfvstats.jpa.Tables.SELECTBF_PLAYERS;
+import static io.github.bfvstats.util.DbUtils.getDslContext;
 
-  Player getPlayer(int id);
+public class PlayerService {
+  public List<Player> getPlayers() {
+    List<Player> players = new ArrayList<>();
+    Result<SelectbfPlayersRecord> records = getDslContext().selectFrom(SELECTBF_PLAYERS).fetch();
+    records.forEach(r -> players.add(toPlayer(r)));
+    return players;
+  }
+
+  public Player getPlayer(int id) {
+    SelectbfPlayersRecord r = getDslContext().selectFrom(SELECTBF_PLAYERS).
+        where(SELECTBF_PLAYERS.ID.eq(id)).fetchOne();
+    return toPlayer(r);
+  }
+
+  private static Player toPlayer(SelectbfPlayersRecord r) {
+    return new Player()
+        .setId(r.getId())
+        .setName(r.getName())
+        .setKeyHash(r.getKeyhash());
+  }
 }
