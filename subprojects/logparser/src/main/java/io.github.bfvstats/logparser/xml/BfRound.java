a50f070706b288d9c97b5c2e450cee39140eca6c
bfstats <bfstatsrefractor@gmail.com>
2016-12-30 03:50:50 +0200

    add helper methods to get setting value from round

--- subprojects/logparser/src/main/java/io.github.bfvstats/logparser/xml/BfRound.java
+++ subprojects/logparser/src/main/java/io.github.bfvstats/logparser/xml/BfRound.java
@@ -53,4 +53,125 @@ public class BfRound {
     }
     return Integer.valueOf(settingValue);
   }
+
+  public String getServerName() {
+    return getSettingValue("server name");
+  }
+
+  public int getPort() {
+    return getIntegerSettingValue("port");
+  }
+
+  public String getModId() {
+    return getSettingValue("modid");
+  }
+
+  public String getGame() {
+    return getSettingValue("mapid");
+  }
+
+  public String getMap() {
+    return getSettingValue("map");
+  }
+
+  public String getGameMode() {
+    return getSettingValue("game mode");
+  }
+
+  public int getMaxGameTime() {
+    return getIntegerSettingValue("gametime");
+  }
+
+  public int getMaxPlayers() {
+    return getIntegerSettingValue("maxplayers");
+  }
+
+  public int getScoreLimit() {
+    return getIntegerSettingValue("scorelimit");
+  }
+
+  public int getNumberOfRoundsPerMap() {
+    return getIntegerSettingValue("norounds");
+  }
+
+  public int getSpawnTime() {
+    return getIntegerSettingValue("spawntime");
+  }
+
+  public int getSpawnDelay() {
+    return getIntegerSettingValue("spawndelay");
+  }
+
+  public int getGameStartDelay() {
+    return getIntegerSettingValue("gamestartdelay");
+  }
+
+  public int getRoundStartDelay() {
+    return getIntegerSettingValue("roundstartdelay");
+  }
+
+  public int getSoldierFriendlyFire() {
+    return getIntegerSettingValue("soldierff");
+  }
+
+  public int getVehicleFriendlyFire() {
+    return getIntegerSettingValue("vehicleff");
+  }
+
+  public int getTicketRatio() {
+    return getIntegerSettingValue("ticketratio");
+  }
+
+  public boolean isTeamKillPunished() {
+    Integer tkpunish = getIntegerSettingValue("tkpunish");
+    return tkpunish.equals(1);
+  }
+
+  public boolean isPunkBusterEnabled() {
+    Integer punkbuster = getIntegerSettingValue("sv_punkbuster");
+    return punkbuster.equals(1);
+  }
+  /*
+
+
+    tkpunish = bfRound.getIntegerSettingValue("tkpunish");
+    Integer deathCameraType = bfRound.getIntegerSettingValue("deathcamtype");
+
+    <bf:setting name="server name">Vietnam Europe CO-OP</bf:setting>
+  <bf:setting name="port">15567</bf:setting>
+  <bf:setting name="modid">bfvietnam</bf:setting>
+  <bf:setting name="mapid">BFVietnam</bf:setting>
+  <bf:setting name="map">khe_sahn</bf:setting>
+  <bf:setting name="game mode">GPM_COOP</bf:setting>
+  <bf:setting name="gametime">35</bf:setting>
+  <bf:setting name="maxplayers">60</bf:setting>
+  <bf:setting name="scorelimit">0</bf:setting>
+  <bf:setting name="norounds">2</bf:setting>
+  <bf:setting name="spawntime">10</bf:setting>
+  <bf:setting name="spawndelay">5</bf:setting>
+  <bf:setting name="gamestartdelay">15</bf:setting>
+  <bf:setting name="roundstartdelay">10</bf:setting>
+  <bf:setting name="soldierff">100</bf:setting>
+  <bf:setting name="vehicleff">100</bf:setting>
+  <bf:setting name="ticketratio">100</bf:setting>
+
+  <bf:setting name="internet">1</bf:setting>
+  <bf:setting name="alliedtr">1</bf:setting>
+  <bf:setting name="axistr">1</bf:setting>
+  <bf:setting name="coopskill">100</bf:setting>
+  <bf:setting name="coopcpu">75</bf:setting>
+  <bf:setting name="allownosecam">1</bf:setting>
+  <bf:setting name="freecamera">0</bf:setting>
+  <bf:setting name="externalviews">1</bf:setting>
+  <bf:setting name="autobalance">0</bf:setting>
+  <bf:setting name="tagdistance">100</bf:setting>
+  <bf:setting name="tagdistancescope">300</bf:setting>
+  <bf:setting name="kickback">0</bf:setting>
+  <bf:setting name="kickbacksplash">0</bf:setting>
+  <bf:setting name="soldierffonsplash">100</bf:setting>
+  <bf:setting name="vehicleffonsplash">100</bf:setting>
+  <bf:setting name="hitindication">1</bf:setting>
+  <bf:setting name="tkpunish">0</bf:setting>
+  <bf:setting name="sv_punkbuster">0</bf:setting>
+   */
 }
