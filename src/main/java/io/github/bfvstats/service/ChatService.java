6b7e86d62073f4535a3f08525a27154e8737f308
bfstats <bfstatsrefractor@gmail.com>
2017-01-04 04:14:42 +0200

    fix chat

--- src/main/java/io/github/bfvstats/service/ChatService.java
+++ src/main/java/io/github/bfvstats/service/ChatService.java
@@ -2,10 +2,7 @@ package io.github.bfvstats.service;
 
 import io.github.bfvstats.model.ChatMessage;
 import org.jooq.Record;
-import org.jooq.Record4;
-import org.jooq.Result;
 
-import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,14 +14,15 @@ import static io.github.bfvstats.util.DbUtils.getDslContext;
 public class ChatService {
 
   public List<ChatMessage> getChatMessages() {
-    Result<Record4<Integer, String, Timestamp, String>> records = getDslContext().select(ROUND_CHAT_LOG.PLAYER_ID, ROUND_CHAT_LOG.MESSAGE, ROUND_CHAT_LOG.EVENT_TIME, PLAYER.NAME)
+    return getDslContext().select(ROUND_CHAT_LOG.PLAYER_ID, ROUND_CHAT_LOG.MESSAGE, ROUND_CHAT_LOG.EVENT_TIME, ROUND_CHAT_LOG.TEAM, PLAYER.NAME)
         .from(ROUND_CHAT_LOG)
         .join(PLAYER).on(PLAYER.ID.eq(ROUND_CHAT_LOG.PLAYER_ID))
         .orderBy(ROUND_CHAT_LOG.EVENT_TIME.sortAsc())
-        .limit(0, 50)
-        .fetch();
-
-    return records.stream().map(this::toChatMessage).collect(Collectors.toList());
+        //.limit(0, 50)
+        .fetch()
+        .stream()
+        .map(this::toChatMessage)
+        .collect(Collectors.toList());
   }
 
   private ChatMessage toChatMessage(Record r) {
