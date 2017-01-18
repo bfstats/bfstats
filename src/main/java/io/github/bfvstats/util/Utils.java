f5239d7d271cd3dac5f3c06988c0d8fef212be89
bfstats <bfstatsrefractor@gmail.com>
2017-01-18 23:45:33 +0200

    dont allow percentage to be negative add table headings

--- src/main/java/io/github/bfvstats/util/Utils.java
+++ src/main/java/io/github/bfvstats/util/Utils.java
@@ -5,6 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Utils {
   public static float percentage(int a, int total) {
+    if (a < 0) {
+      // would make much sense to have a negative percent
+      return 0;
+    }
+
     if (total == 0) {
       if (a != 0) {
         log.warn("total is 0, but a is " + a);
