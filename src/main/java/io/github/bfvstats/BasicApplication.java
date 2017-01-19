eb1c59d9c46dc51f596fe0bca3a11db19716f53f
bfstats <bfstatsrefractor@gmail.com>
2017-01-19 20:33:02 +0200

    fallback to classpath loader (e.g for errors)

--- src/main/java/io/github/bfvstats/BasicApplication.java
+++ src/main/java/io/github/bfvstats/BasicApplication.java
@@ -1,11 +1,12 @@
 package io.github.bfvstats;
 
-import com.google.common.collect.Lists;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.mitchellbosecke.pebble.PebbleEngine;
+import com.mitchellbosecke.pebble.loader.ClasspathLoader;
 import com.mitchellbosecke.pebble.loader.DelegatingLoader;
 import com.mitchellbosecke.pebble.loader.FileLoader;
+import com.mitchellbosecke.pebble.loader.Loader;
 import io.github.bfvstats.controller.*;
 import io.github.bfvstats.pebble.Java8Extension;
 import io.github.bfvstats.util.DbUtils;
@@ -16,6 +17,8 @@ import ro.pippo.guice.GuiceControllerFactory;
 import ro.pippo.pebble.PebbleTemplateEngine;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BasicApplication extends ControllerApplication {
 
@@ -28,13 +31,23 @@ public class BasicApplication extends ControllerApplication {
       fileLoader.setCharset(PippoConstants.UTF8);
 
       if (application.getPippoSettings().isDev()) {
+        List<Loader<?>> loaders = new ArrayList<>();
+
         String templatesAbsolutePath = Paths.get(".").toAbsolutePath().normalize()
             .resolve("src\\main\\resources\\templates")
             .normalize()
             .toString();
         fileLoader.setPrefix(templatesAbsolutePath);
         fileLoader.setSuffix("." + "peb");
-        builder.loader(new DelegatingLoader(Lists.newArrayList(fileLoader)));
+        loaders.add(fileLoader);
+
+        ClasspathLoader classpathLoader = new ClasspathLoader();
+        classpathLoader.setCharset(PippoConstants.UTF8);
+        classpathLoader.setPrefix("templates/");
+        classpathLoader.setSuffix(".peb");
+        loaders.add(classpathLoader);
+
+        builder.loader(new DelegatingLoader(loaders));
       }
     }
   }
