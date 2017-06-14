308b7c5cffbeb91cbdf44a671484687fc417f2d6
bfstats <bfstatsrefractor@gmail.com>
2017-06-15 01:17:39 +0300

    fix message in .sh scripts
    move (unused) dbcreator.sh to jooq_gen

--- dbfiller.sh
+++ dbfiller.sh
@@ -3,6 +3,6 @@ mainClass="io.github.bfvstats.dbfiller.DbFiller"
 
 ./gradlew :$projectPath:build :$projectPath:copyToLib
 
-echo Now starting to run %projectPath%
+echo Now starting to run $projectPath
 
 java -classpath $projectPath/lib/*:$projectPath/build/classes/main:$projectPath/build/resources/main $mainClass
--- webapp.sh
+++ webapp.sh
@@ -3,6 +3,6 @@ mainClass="io.github.bfvstats.Launcher"
 
 ./gradlew :$projectPath:build :$projectPath:copyToLib
 
-echo Now starting to run %projectPath%
+echo Now starting to run $projectPath
 
 java -classpath $projectPath/lib/*:$projectPath/build/classes/main:$projectPath/build/resources/main $mainClass
